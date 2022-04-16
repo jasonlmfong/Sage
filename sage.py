3d017151b6cf9ee8ec8f78c8adff88180a2e890a
jasonlmfong <jasonlmfong@gmail.com>
2022-04-16 16:46:32 -0400

    added postgresql db connection

--- sage.py
+++ sage.py
@@ -2,48 +2,60 @@ import discord
 import json
 import logging
 import random
+import psycopg2
+from datetime import datetime
 
-#open json file for the auth tokens
+# open json file for the auth tokens
 with open('auth.json') as auth:
     tokens = json.load(auth)
 
-#create client
+# create client
 client = discord.Client()
 
-#message upon launch
+# message upon launch
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
-#react to user saying hello
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+# PostgreSQL db connection
+conn = psycopg2.connect(database="postgres", user = tokens['pguser'], password = tokens['pgpw'], host = "127.0.0.1", port = "5432")
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+if conn:
+    print("Opened database successfully")
 
-#logger
+cur = conn.cursor()
+cur.execute('CREATE TABLE IF NOT EXISTS Users (\
+            User_ID int PRIMARY KEY, \
+            Username VARCHAR NOT NULL, \
+            Wealth int NOT NULL, \
+            Created_on TIMESTAMP NOT NULL\
+            );')
+conn.commit()
+print("Table Users created successfully")
+
+# logger
 logger = logging.getLogger('discord')
 logger.setLevel(logging.DEBUG)
 handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
 
+## for other logs use the following
+## logger.error(f"error happened heres the message {message}")
+
 @client.event
 async def on_message(message):
+    # log all messages
     logger.info(message)
-
-##for other logs use the following
-##logger.error(f"error happened heres the message {message}")
-
-
-#roll a die
-@client.event
-async def on_message(message):
+    # do nothing on ignore bot messages
     if message.author == client.user:
         return
+    
+    #react to user saying hello
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+    #roll a n sided die
     if message.content.startswith('$d'):
         max = message.content[2:]
         if max.isnumeric() and int(max) > 0:
@@ -52,19 +64,12 @@ async def on_message(message):
         else:
             await message.channel.send(f'{max} is not a value number of sides of a die')
 
-#flip a coin
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+    #flip a coin
     if message.content.startswith('$coin'):
         outcome = random.randint(1, 2)
         coin = {1: 'heads', 2: 'tails'}
         await message.channel.send(f'Coin returned {coin[outcome]}')
-
-
+    
 
 #run the bot using token
 client.run(tokens['bot token'])
-
-
